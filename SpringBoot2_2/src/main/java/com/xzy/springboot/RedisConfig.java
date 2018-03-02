4336fb3829a3abe51da78f9fffc60b844c8c055b
Fare丶梦 <1269131995@qq.com>
2018-03-02 11:35:39 +0800

    SpringBoot整合redis由于没有redis环境所以没有测试

---
+++ SpringBoot2_2/src/main/java/com/xzy/springboot/RedisConfig.java
@@ -0,0 +1,35 @@
+package com.xzy.springboot;
+
+import com.xzy.springboot.entity.User;
+import com.xzy.springboot.serializer.RedisObjectSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author XuZiyu
+ * @date 2018-3-29:46
+ * @description
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, User> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, User> template = new RedisTemplate<String, User>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new RedisObjectSerializer());
+        return template;
+    }
+
+
+}
---
+++ SpringBoot2_2/src/main/java/com/xzy/springboot/entity/User.java
@@ -0,0 +1,37 @@
+package com.xzy.springboot.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author XuZiyu
+ * @date 2018-3-29:44
+ * @description
+ */
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -1L;
+
+    private String username;
+    private Integer age;
+
+    public User(String username, Integer age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
