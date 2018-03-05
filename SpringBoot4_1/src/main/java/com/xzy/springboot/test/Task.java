7cc17a46e390ee40b034ef2c7d9f539a1e37292c
Fare丶梦 <1269131995@qq.com>
2018-03-05 15:34:45 +0800

    @Async实现异步调用

---
+++ SpringBoot4_1/src/main/java/com/xzy/springboot/test/Task.java
@@ -0,0 +1,72 @@
+package com.xzy.springboot.test;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+import java.util.concurrent.Future;
+
+/**
+ * @author XuZiyu
+ * @date 2018-3-515:15
+ * @description
+ */
+@Component
+public class Task {
+    public static Random random =new Random();
+
+    @Async
+    public void doTaskOne() throws Exception {
+        System.out.println("开始做任务一");
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(10000));
+        long end = System.currentTimeMillis();
+        System.out.println("完成任务一，耗时：" + (end - start) + "毫秒");
+    }
+    @Async
+    public void doTaskTwo() throws Exception {
+        System.out.println("开始做任务二");
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(10000));
+        long end = System.currentTimeMillis();
+        System.out.println("完成任务二，耗时：" + (end - start) + "毫秒");
+    }
+    @Async
+    public void doTaskThree() throws Exception {
+        System.out.println("开始做任务三");
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(10000));
+        long end = System.currentTimeMillis();
+        System.out.println("完成任务三，耗时：" + (end - start) + "毫秒");
+    }
+
+    @Async
+    public Future<String> doTaskOneNew() throws Exception{
+        System.out.println("开始做任务一");
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(10000));
+        long end = System.currentTimeMillis();
+        System.out.println("完成任务一，耗时：" + (end - start) + "毫秒");
+        return new AsyncResult<>("任务一完成");
+    }
+
+    @Async
+    public Future<String> doTaskTwoNew() throws Exception {
+        System.out.println("开始做任务二");
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(10000));
+        long end = System.currentTimeMillis();
+        System.out.println("完成任务二，耗时：" + (end - start) + "毫秒");
+        return new AsyncResult<>("任务二完成");
+    }
+    @Async
+    public Future<String> doTaskThreeNew() throws Exception {
+        System.out.println("开始做任务三");
+        long start = System.currentTimeMillis();
+        Thread.sleep(random.nextInt(10000));
+        long end = System.currentTimeMillis();
+        System.out.println("完成任务三，耗时：" + (end - start) + "毫秒");
+        return new AsyncResult<>("任务三完成");
+    }
+}
